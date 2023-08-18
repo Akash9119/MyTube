d9eb3bed0a83d7c445fda7faf81f58f25d491dce
Akash <akashj.vasava@gmail.com>
2023-08-18 17:29:44 +0530

    Updated ContextApi file

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,57 +1,31 @@
-// import React, { createContext, useState, useEffect } from "react";
-
-// import { fetchDataFromApi } from "../utils/api";
-// export const Context = createContext();
-
-// export const AppContext = (props) => {
-//     const [loading, setLoading] = useState(false);
-//     const [searchResults, setSearchResults] = useState([]);
-//     const [selectedCategory, setSelectedCategory] = useState("New");
-//     const [mobileMenu, setMobileMenu] = useState(false);
-
-//     useEffect(() => {
-//         fetchSelectedCategoryData(selectedCategory);
-//     }, [selectedCategory]);
-
-//     const fetchSelectedCategoryData = (query) => {
-//         setLoading(true);
-//         fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
-//             setSearchResults(contents);
-//             setLoading(false);
-//         });
-//     };
-
-//     return (
-//         <Context.Provider
-//             value={{
-//                 loading,
-//                 setLoading,
-//                 searchResults,
-//                 selectedCategory,
-//                 setSelectedCategory,
-//                 mobileMenu,
-//                 setMobileMenu,
-//             }}
-//         >
-//             {props.children}
-//         </Context.Provider>
-//     );
-// };
-
-
-// AppContext.js
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { SampleResponse } from "./SampleResponse"; // Import the data array
+import { fetchDataFromApi } from "../utils/api"; // Import the API fetching function
 
 export const Context = createContext();
 
 export const AppContext = (props) => {
     const [loading, setLoading] = useState(false);
-    const [searchResults, setSearchResults] = useState(SampleResponse); // Use videoData array
+    const [searchResults, setSearchResults] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("New");
     const [mobileMenu, setMobileMenu] = useState(false);
 
-    // No need for useEffect and fetchDataFromApi here
+    useEffect(() => {
+        // Comment out the API fetching code for now
+        // fetchSelectedCategoryData(selectedCategory);
+
+        // Instead, use the sample data from the array
+        setSearchResults(SampleResponse);
+    }, [selectedCategory]);
+
+    // Comment out the fetchSelectedCategoryData function for now
+    // const fetchSelectedCategoryData = (query) => {
+    //     setLoading(true);
+    //     fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+    //         setSearchResults(contents);
+    //         setLoading(false);
+    //     });
+    // };
 
     return (
         <Context.Provider
